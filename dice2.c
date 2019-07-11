af95cc4eaa19001d9d15fb11dfb7e2369ef9b948
s1260020 <52784907+s1260020@users.noreply.github.com>
2019-07-11 22:20:51 +0900

    Create dice2.c

---
+++ dice2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void){
+  int A=0;
+  int B=0;
+  int total=0;
+  char str[15];
+  srand(time(NULL));
+  printf("What is your name?\n");
+  scanf("%s",str);
+  printf("Hello,%s!\n",str);
+  A = rand()%6+1;
+  B = rand()%6+1;
+  total = A+B;
+  printf("Die 1:%d\n",A);
+  printf("Die 2:%d\n",B);
+  printf("Total :%d\n",total);
+  if(total>7){
+  printf("you won!");
+  }
+  else {
+  printf("you lost!");
+  }
+  
+}
