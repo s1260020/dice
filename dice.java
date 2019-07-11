35146a712dc0309f198993806ad6dd666339cff8
s1260020 <52784907+s1260020@users.noreply.github.com>
2019-07-11 21:38:18 +0900

    Create dice.java

---
+++ dice.java
@@ -0,0 +1,13 @@
+public class Dice{
+public static void main(Strin[] args){
+int A=0;
+int B=0;
+int total=0;
+A = (int)(Math.ramdom()*6)+1;
+B = (int)(Math.ramdom()*6)+1;
+total = A+B;
+System.out.println("Die1:"+A);
+System.out.println("Die2:"+B);
+System.out.println("Total:"+total);
+}
+}
